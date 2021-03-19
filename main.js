947388a9beb0ab158b7a78779735ab83bbbb79e6
Poshika-alt <76203042+Poshika-alt@users.noreply.github.com>
2021-03-19 19:46:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var SpeechRecognition = window.webkitSpeechRecgonition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    speak();
+    document.getElementById("textbox").innerHTML = Content;
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("text").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+camera = document.getElementById("camera");
